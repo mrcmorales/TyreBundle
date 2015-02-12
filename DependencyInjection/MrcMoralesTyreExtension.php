60ee99b67b07ff122d580fb96700193482250022
Marc Morales Valldepérez <marcmorales83@gmail.com>
2015-02-12 12:56:00 +0100

    code style updated

--- DependencyInjection/MrcMoralesTyreExtension.php
+++ DependencyInjection/MrcMoralesTyreExtension.php
@@ -5,7 +5,6 @@ namespace MrcMorales\TyreBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-
 /**
  * This is the class that loads and manages your bundle configuration
  *
