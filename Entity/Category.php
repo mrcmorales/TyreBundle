665488b26dac564084d695e41ba5b2293333c166
Yosvel Quintero Arguelles <yosvelquintero@gmailcom>
2015-06-08 16:19:53 +0200

    Added Category model to TyreBundle

---
+++ Entity/Category.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Tyre Bundle for Symfony2
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author: Yosvel Quintero <yosvelquintero@gmail.com>
+ *
+ * Date: 08/06/15
+ * Time: 18:48
+ */
+namespace MrcMorales\TyreBundle\Entity;
+
+use MrcMorales\TyreBundle\Model\Category as BaseCategory;
+
+/**
+ * Category
+ */
+class Category extends BaseCategory
+{
+}
---
+++ Model/Category.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Tyre Bundle for Symfony2
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author: Yosvel Quintero <yosvelquintero@gmail.com>
+ *
+ * Date: 08/06/15
+ * Time: 17:23
+ */
+namespace MrcMorales\TyreBundle\Model;
+
+/**
+ * Category
+ */
+class Category extends AbstractBase
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
